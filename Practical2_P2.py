f544ff2fac0bdc8d8254ee8b8c04984dda75957d
sakshi-nimje <115687946+sakshi-nimje@users.noreply.github.com>
2024-04-15 22:21:51 +0530

    Add files via upload

---
+++ Practical2_P2.py
@@ -0,0 +1,5 @@
+import pandas as pd
+score=pd.read_excel('scores.xlsx')
+print("Reading an Excel datasheet :  ")
+print('-------------------------------------------------------------------')
+print(score)
